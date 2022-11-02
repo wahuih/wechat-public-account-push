444b09acb09415744fbeb0b65eb8c62ff7d4a456
wahuih <87839927+wahuih@users.noreply.github.com>
2022-11-02 22:55:12 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -11,8 +11,8 @@ const USER_CONFIG = {
 
   APP_SECRET: '983567c4a2c12c00e6c42743b304e8e1',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '江西',
+  CITY: '共青城',
 
   USERS: [
     {
@@ -26,39 +26,39 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '小唐', year: '2003', date: '11-02',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '李四', year: '1996', date: '11-03',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2020', date: '11-03',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2022-10-25' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+      //  { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'H-hPX-5FEfI7SJuqXdTCsCgC7YAmfa8OOnXmAM6AzJQ',
+ // CALLBACK_TEMPLATE_ID: 'H-hPX-5FEfI7SJuqXdTCsCgC7YAmfa8OOnXmAM6AzJQ',
 
-  CALLBACK_USERS: [
-    {
-      name: '自己',
+ // CALLBACK_USERS: [
+ //   {
+   //   name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'gh_0832e666fd42',
-    }
-  ],
+   //   id: 'gh_0832e666fd42',
+  //  }
+//  ],
 
-}
+//}
 
 module.exports = USER_CONFIG
 
